9980eba3b222d84d50eac9769f964b4ad82bcdd2
Guy-David Bru <guy-david.bru@philamuseum.org>
2023-03-19 15:07:01 -0400

    ScrollTrigger and locomotive scroll for smooth scrolling (GSAP plugin is not free)

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Home from "./Pages/Home";
+import useLocoScroll from './Components/useLocoScroll';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+export default function App() {
+  
+  const smoothScroll = useLocoScroll(true);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div data-scroll-container className="App">
+      <main data-scroll-section>
+        <Home />
+        <section className="section" >
+          <div className="content">
+            <h2>some other title</h2>
+            <p>Some other paragraph</p>
+          </div>
+          <div className="ledge bg-dark"></div>
+        </section>
+        <section className="section bg-dark" >
+          <div className="content">
+            <h2>End of page</h2>
+            <p>Some other paragraph</p>
+          </div>
+        </section>
+      </main>
     </div>
   )
-}
 
-export default App
+}
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+export default function Home(){
+
+  return (
+    <div className="home">
+      <section >
+        <h1>Guyda</h1>
+      </section>
+      <section >
+        <h2>Crafting the web from top to bottom, <br/>one click at a time.</h2>
+        <p>As a full-stack web developer, I bring a unique combination of technical and creative skills to the table, allowing me to create dynamic and engaging websites that meet the needs of both users and clients.</p>
+      </section>
+    </div>
+  )
+}
