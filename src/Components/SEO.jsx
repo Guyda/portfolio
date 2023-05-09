777b28ea5fbdf4f8193870dd7e703cf2cd7d27ab
Guy-David Bru <guy-david.bru@philamuseum.org>
2023-05-09 10:19:53 -0400

    replace favicon

--- src/Components/SEO.jsx
+++ src/Components/SEO.jsx
@@ -4,7 +4,7 @@ export default function SEO({ title, description, name, type }) {
   return (
     <Helmet prioritizeSeoTags>
       <meta charset="UTF-8" />
-      <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {/* Standard metadata tags */}
       <title>{title}</title>
